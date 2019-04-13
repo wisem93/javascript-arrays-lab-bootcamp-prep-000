1e27249d23d72e5653fb9d983c0941111c4322d5
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-13 16:28:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  kittens.
+  kittens.push(name);
+  return kittens;
 }
